7fee62ee435ff630eaf204ecb3c4ae25326b0326
Wind-Wing <windwing.me@gmail.com>
2021-09-01 19:46:47 +0800

    Write main.m for the whole pipeline of spliting channels and sorting

--- main.m
+++ main.m
@@ -1,8 +1,9 @@
-%% Split Channels
+%% Set Constants
 path = "/media/wind/Data Disk/intan/CA_sleep_210822_102231/";
 file = "CA_sleep_210822_102231.rhd";
 
-% Three probes
+%% Split Channels
+Three probes
 channel_1 = [10, 8, 24, 18, 22, 16, 11, 6, 23, 20, 7, 19, 21, 9, 17, 5] + 1;
 channel_2 = [13, 4, 1, 26, 34, 12, 31, 2, 30, 28, 0, 14, 19, 3, 25, 27] + 1;
 channel_3 = [41, 43, 46, 35, 39, 42, 15, 45, 32, 33, 47, 40, 36, 44, 38, 37] + 1;
@@ -27,3 +28,10 @@ fwrite(fid, amplifier_data(channel_3, :), 'int16');
 fclose(fid);
 
 %% Sorting Spikes
+run("./config.m")
+
+for i=['1','2','3']
+    ops.fbinary = path + "sorting/" + i + "/";
+    ops.root = ops.root;
+    run_kilosort(ops);
+end
